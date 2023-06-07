bd70684e03dc5d3522753856673a36b239ec2de3
MihailEro <mihailerohin235@gmail.com>
2023-06-07 22:07:20 +0300

    write tests

--- tests/test_item.py
+++ tests/test_item.py
@@ -22,3 +22,20 @@ def test_item_apply_discount(test_item):
     Item.pay_rate = 0.9
     test_item.apply_discount()
     assert test_item.price == 90
+
+
+def test_item_property(test_item):
+    test_item.name = 'phone'
+    assert test_item.name == 'phone'
+
+
+def test_item_istantiate_from_csv(test_item):
+    test_item.instantiate_from_csv()
+    assert test_item.all[0].name == "Смартфон"
+    assert test_item.all[0].price == "100"
+    assert test_item.all[0].quantity == "1"
+
+
+def test_string_to_number(test_item):
+    assert test_item.string_to_number('1357') == 1357
+    assert test_item.string_to_number('12.75') == 12
